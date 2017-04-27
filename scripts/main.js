0b2fdc219c7d7b4c05053dfce9b501bdca067b55
Will Moritz <will@crowdfavorite.com>
2017-04-26 23:08:32 -0400

    night out prototype

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
+  var $nextButton = $('.module-sequence-footer-button--next');
+  var nightOutItemsMin = 3;
+
   if ($('#alc-learn--night-out').length) {
     // Night out script
-    dragula([document.querySelector('#left'), document.querySelector('#right')]);
 
+    // Hide the next button
+    $nextButton.hide();
+
+    // Make the elements draggable
+    var drake = dragula([document.querySelector('#alc-learn--night-out__right'), document.querySelector('#alc-learn--night-out__left')]);
+
+    // On every drop, if bag has enough element, show next button.
+    drake.on('drop', function(el, target, source, sibling) {
+      if ($('#alc-learn--night-out__left').children().length > nightOutItemsMin) {
+        $nextButton.show();
+      }
+    });
   }
 
   if ($('#alc-learn--dialogue').length) {
