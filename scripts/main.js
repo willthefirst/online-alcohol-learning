b0d9c7b80b928670d0f970925a379fab034e8bae
Will Moritz <will@crowdfavorite.com>
2017-05-18 13:52:55 -0400

    updated word cloud for mad libs structure

--- scripts/main.js
+++ scripts/main.js
@@ -2,35 +2,51 @@ $(document).ready(function() {
   var $nextButton = $('.module-sequence-footer-button--next');
   var nightOutItemsMin = 3;
 
-  if ($('#alc-learn--night-out').length) {
-    // Night out script
-
+  // Night out script
+  if ($('.alc-learn--night-out').length) {
     // Hide the next button
     $nextButton.hide();
 
     // Make the elements draggable
-    var drake = dragula([document.querySelector('#alc-learn--night-out__right'), document.querySelector('#alc-learn--night-out__left')]);
+    var drake = dragula([document.querySelector('.alc-learn--night-out__word-cloud'), document.querySelector('.alc-learn--night-out__response')]);
 
     // On every drop, if bag has enough element, show next button.
     drake.on('drop', function(el, target, source, sibling) {
-      if ($('#alc-learn--night-out__left').children().length > nightOutItemsMin) {
+      if ($('.alc-learn--night-out__response').children().length > nightOutItemsMin) {
+        alert('Success!')
         $nextButton.show();
       }
     });
   }
 
+  // Dialogue script
   if ($('#alc-learn--dialogue').length) {
-    // Dialogue script
-    var dialogue = {
-      comp: {
-        d1: 'Want to have sex?'
+    // Hide the next button
+    $nextButton.hide();
+    var $compDialogue = $('#alc-learn--dialogue__comp');
+    var $userDialogue = $('#alc-learn--dialogue__user');
 
-      },
-      user: {
-        d2: [{
+    // var dialogue = [
+    //   {
+    //     m: 'Comp: question 1'
+    //     answers: [
+    //       {
+    //         m: 'Yes'
+    //         next: 'Yes > Question 2'
+    //       }, {
+    //         m: 'No',
+    //         next: 'No > Question 2'
+    //       }, {
+    //         m: 'Maybe',
+    //         next: 'Yes > Question 2'
+    //       }
+    //     ]
+    //   }
+    // ]
 
-        }]
-      }
-    }
+
+    $compDialogue.html('Comp 1');
   }
+
+
 });
